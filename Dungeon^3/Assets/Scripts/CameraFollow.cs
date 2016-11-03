d2ebc8035ac9fbf08265147afcf7bd81f9e50fb7
Ironbizmark <anna.arpacidusseau@gmail.com>
2016-11-02 21:22:15 -0500

    Mouse problem messed with

---
+++ Dungeon^3/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;       
+
+	//x = -20, y = 24, z = 20
+	private Vector3 offset = new Vector3 (-20, 24,20);       
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
--- Dungeon^3/Assets/Scripts/PlayerController.cs
+++ Dungeon^3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 	private Vector3 camForward;
 	private Transform cam;
 	private Vector3 move;
+	public float damping = 1;
+	public float gravity = 100.0F;
 
 	private Rigidbody rBody;
 
@@ -27,8 +29,29 @@ public class PlayerController : MonoBehaviour {
 		float inputZ = Input.GetAxis("Vertical");
 		moveDirection = (inputZ*camForward + inputX*cam.right).normalized;
 		moveDirection *= speed;
+		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 
+
+		Vector3 pos = new Vector3(Input.mousePosition.x - 500,0,Input.mousePosition.y - 300);
+
+		//transform.LookAt(Vector3.Scale(Camera.main.ScreenToWorldPoint(Input.mousePosition) , noY));
+		transform.LookAt(pos);
+
+		//TODO: This kind of works... Think about actually fixing the problem though...
+
+
+		/*
+
+		Debug.Log(Input.mousePosition);
+		Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		if(Input.GetMouseButtonDown(0)) {
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.transform.position = pos;
+		}
+		Debug.Log(Vector3.Scale(Camera.main.ScreenToWorldPoint(Input.mousePosition) , noY));
+
+		*/
 
 	}
 		
