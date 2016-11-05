437bc65d3d093dfe337504df1baad130c07391a0
Ironbizmark <anna.arpacidusseau@gmail.com>
2016-11-05 17:07:45 -0500

    Updated Health

---
+++ Dungeon^3/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+	public int health = 100;
+	private Text healthDisplay;
+	// Use this for initialization
+	void Start () {
+		healthDisplay = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthDisplay.text = "Health: " + health + "/100";
+	}
+
+	public void UpdateHealth(int val) {
+		health += val;
+	}
+}
