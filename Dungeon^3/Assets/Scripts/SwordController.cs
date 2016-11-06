0f5515b39b7f33e330594990507cd90a0cc0596b
Langston Nashold <langston201@gmail.com>
2016-11-05 19:00:12 -0500

    Added sword movement

--- Dungeon^3/Assets/Scripts/SwordController.cs
+++ Dungeon^3/Assets/Scripts/SwordController.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class SwordController : MonoBehaviour {
-
+    public bool isStabbing;
+    private int ticksSinceStart;
 	// Use this for initialization
 	void Start () {
-	
+        isStabbing = false;
+        ticksSinceStart = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetMouseButtonDown(1)&&!isStabbing)
+        {
+            isStabbing = true;
+            ticksSinceStart = 0;
+        }
+        if(isStabbing)
+        {
+            if (ticksSinceStart < 20)
+            {
+                transform.Rotate(3, 0, 0);
+            }
+            else if (ticksSinceStart >= 20 && ticksSinceStart < 40)
+            {
+                transform.Rotate(-3, 0, 0);
+            }
+            else
+            {
+                isStabbing = false;
+            }
+            ticksSinceStart++;
+        }
+    
 	}
+
 }
