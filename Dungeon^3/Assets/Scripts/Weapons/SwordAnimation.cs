9d534cb45605d725cbe690fe50e0780fcaa51c31
Langston Nashold <langston201@gmail.com>
2016-11-22 22:41:11 -0600

    Added a rudimentary slash animation
    Added a slash animation that is activated by right click. Whether it is
    slashing or not is controlled by animator tab. To change the way it
    slashes update the swing.anim file. Also somewhere along the line damage
    sword damage got broken.

---
+++ Dungeon^3/Assets/Scripts/Weapons/SwordAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordAnimation : MonoBehaviour {
+	
+
+	
+	 void Update(){
+	 if (Input.GetMouseButtonDown(1))
+     {
+         GetComponent<Animator>().SetTrigger("RightClick");
+     }
+	}
+	 
+	
+}
