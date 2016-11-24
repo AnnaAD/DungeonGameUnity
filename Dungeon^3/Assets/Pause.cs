2947a17eb3f7d11b049befcae15c6c736f80e863
Langston Nashold <langston201@gmail.com>
2016-11-23 20:30:05 -0600

    Pause Menu
    Added a Menu with no functionality to pausing

--- Dungeon^3/Assets/Pause.cs
+++ Dungeon^3/Assets/Pause.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Pause : MonoBehaviour {
 	public bool isPaused;
+	public GameObject menu;
 	// Use this for initialization
 	void Start () {
-	
+		menu = GameObject.Find ("PauseMenu");
+		menu.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -20,9 +22,11 @@ public class Pause : MonoBehaviour {
 		if (!isPaused) {
 			isPaused = true;
 			Time.timeScale = 0.0f;
+			menu.SetActive (true);
 		} else {
 			isPaused = false;
 			Time.timeScale = 1.0f;
+			menu.SetActive (false);
 		}
 	}
 }
