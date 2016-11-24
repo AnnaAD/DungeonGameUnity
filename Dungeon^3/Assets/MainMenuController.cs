878f7c7b6ead1708237df08ff16b80aa6f5ec5e3
Langston Nashold <langston201@gmail.com>
2016-11-23 22:22:44 -0600

    Basic Start Menu
    A basic start menu, with buttons for playing the first level and
    quitting. IT could look nicer and it doesn't have a logo or the name of
    the game anywhere. Also could add loading a game, or jumping to a
    specific level depending on how we want to design the game. The tutorial
    could also be a level itself.

---
+++ Dungeon^3/Assets/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Play()
+	{
+		Application.LoadLevel ("dungeon");
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+}
