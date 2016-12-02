b6bfe66cf83476d8b957a2e847d7b5370e980ebc
Langston Nashold <langston201@gmail.com>
2016-12-01 20:34:30 -0600

    Non-Functional Inventory
    It can slide up and down, by pressing on the little button that says
    inventory. Probably going to go back and tweak the looks a bit it's
    kinda ugly right now. All images were made with Gimp, saved in a png and
    the default gimp save. Also moved all the builds to a folder.

---
+++ Dungeon^3/Assets/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	public static bool inventoryOpen;
+	public GameObject inventoryBackground;
+	public GameObject inventoryButton;
+	public void Start(){
+		inventoryOpen = true;
+		toggleInventory ();
+
+	}
+	public void toggleInventory()
+	{	
+		if(inventoryOpen){
+			inventoryButton.GetComponent<Transform>().Translate( new Vector3 (0f, -180.4f, 0f));
+			inventoryBackground.SetActive (false);
+			inventoryOpen = false;
+
+		} else {
+			inventoryButton.GetComponent<Transform> ().Translate(new Vector3 (0f, 180.4f, 0f));
+
+			inventoryBackground.SetActive (true);
+			inventoryOpen = true;
+		}
+	}
+}
