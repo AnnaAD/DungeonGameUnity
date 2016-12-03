7cb68ecfb8c7e773c0c87e697fcadff0c164acd7
Langston Nashold <langston201@gmail.com>
2016-12-03 15:34:23 -0600

    Edited Sword Animation

--- Dungeon^3/Assets/Scripts/Weapons/SwordAnimation.cs
+++ Dungeon^3/Assets/Scripts/Weapons/SwordAnimation.cs
@@ -4,12 +4,25 @@ using System.Collections;
 public class SwordAnimation : MonoBehaviour {
 	
 
-	
+	public bool isSwinging;
+	public void Start()
+	{
+		isSwinging = false;
+	}
 	 public void Swing(){
 	 if (Input.GetMouseButtonDown(1))
      {
          GetComponent<Animator>().SetTrigger("RightClick");
      }
+	}
+
+	public void toggleIsSwinging()
+	{
+		if (isSwinging) {
+			isSwinging = false;
+		} else {
+			isSwinging = true;
+		}
 	}
 	 
 	
