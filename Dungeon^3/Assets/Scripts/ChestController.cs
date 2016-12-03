6664a613e9ea61c6efc38f5a72e14116a5f1d853
Ironbizmark <anna.arpacidusseau@gmail.com>
2016-12-03 09:20:06 -0600

    Anna tries a command-line commit. And recovers the chest.

---
+++ Dungeon^3/Assets/Scripts/ChestController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestController : MonoBehaviour {
+
+	Animator animator;
+	private bool chestOpen;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		chestOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E)) {
+			if(chestOpen == false) {
+				animator.SetTrigger("Open");
+				Debug.Log("trying to open");
+				chestOpen = true;
+			} else {
+				animator.SetTrigger("Close");
+				Debug.Log("trying to close");
+				chestOpen = false;
+			}
+		}
+	}
+}
