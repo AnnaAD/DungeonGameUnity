57a28750e5821db0a2af60b2ebbb6fe8142e5798
Langston Nashold <langston201@gmail.com>
2016-12-03 19:14:55 -0600

    Final Trapdoor Prefab

---
+++ Dungeon^3/Assets/DistanceDetection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceDetection : MonoBehaviour {
+	public GameObject trapdoor;
+	public GameObject player;
+	public float triggerDistance;
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform trans1 = player.GetComponent<Transform>();
+		Transform trans2 = trapdoor.GetComponent<Transform>();
+		if (Mathf.Abs (trans1.position.x - trans2.position.x) < triggerDistance && Mathf.Abs (trans1.position.y - trans1.position.y)<triggerDistance) {
+			animator.SetTrigger ("open");
+		}
+	}
+}
