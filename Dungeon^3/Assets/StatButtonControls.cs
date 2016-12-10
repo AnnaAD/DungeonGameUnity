2ab10382fa78eab93482eab664c7ed33462c1d75
Ironbizmark <anna.arpacidusseau@gmail.com>
2016-12-10 14:29:23 -0600

    Working Stat Updates
    You can now update one stat after leveling.

---
+++ Dungeon^3/Assets/StatButtonControls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatButtonControls : MonoBehaviour {
+	[SerializeField]
+	private GameObject player;
+	private GameObject levelPanel;
+	private GameObject statDisplay;
+
+	void Start() {
+		player = GameObject.Find("Player");
+		statDisplay = GameObject.Find("StatText");
+	}
+
+	public void healthBoostClicked() {
+		player.GetComponent<PlayerStats>().updateHealthBoost(1);
+		gameObject.SetActive(false);
+		statDisplay.GetComponent<StatUI>().updateStats();
+	}
+
+	public void speedClicked() {
+		player.GetComponent<PlayerStats>().updateSpeed(1);
+		gameObject.SetActive(false);
+		statDisplay.GetComponent<StatUI>().updateStats();
+	}
+
+	public void vitalityClicked() {
+		player.GetComponent<PlayerStats>().updateVitality(1);
+		gameObject.SetActive(false);
+		statDisplay.GetComponent<StatUI>().updateStats();
+	}
+
+	public void dexterityClicked() {
+		player.GetComponent<PlayerStats>().updateDexterity(1);
+		gameObject.SetActive(false);
+		statDisplay.GetComponent<StatUI>().updateStats();
+	}
+
+	public void swordsmanshipClicked() {
+		player.GetComponent<PlayerStats>().updateSwordsmanship(1);
+		gameObject.SetActive(false);		
+		statDisplay.GetComponent<StatUI>().updateStats();
+	}
+
+	public void bowmanshipClicked() {
+		player.GetComponent<PlayerStats>().updateBowmanship(1);
+		gameObject.SetActive(false);
+		statDisplay.GetComponent<StatUI>().updateStats();
+	}
+}
