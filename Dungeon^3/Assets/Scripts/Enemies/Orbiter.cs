fadc69d89b8b12f84827938458e33794712b50ea
Langston Nashold <langston201@gmail.com>
2017-01-07 23:50:11 -0600

    Started Orbiter Enemy, will finish later

---
+++ Dungeon^3/Assets/Scripts/Enemies/Orbiter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbiter : Enemy {
+	public GameObject[] bullets;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
