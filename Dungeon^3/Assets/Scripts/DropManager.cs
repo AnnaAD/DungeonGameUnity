1250738404792d5a317a1f4cac3cb410d8536fdc
Ironbizmark <anna.arpacidusseau@gmail.com>
2017-01-08 19:20:14 -0600

    Organized and started thinking about a drop manager

---
+++ Dungeon^3/Assets/Scripts/DropManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropManager : MonoBehaviour {
+	private ArrayList drops;
+
+	// Use this for initialization
+	void Start () {
+		drops = new ArrayList();
+	}
+	
+	public void getDrop(double enemyLevel) {
+
+	}
+
+	public void addDrop(GameObject drop) {
+		drops.Add(drop);
+	}
+
+
+}
