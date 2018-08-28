3946926a9b68507292a84cae04cba98c43943bfd
lgnashold <lgnashold@gmail.com>
2018-08-28 17:24:43 -0500

    Fixing everything?

--- Dungeon^3/Assets/Scripts/Inventory/PopupManager.cs
+++ Dungeon^3/Assets/Scripts/Inventory/PopupManager.cs
@@ -7,18 +7,18 @@ public class PopupManager : MonoBehaviour {
 	public GameObject inventoryButton;
 	public void Start(){
 		inventoryOpen = true;
-		toggleInventory ();
+		//toggleInventory ();
 
 	}
 	public void toggleInventory()
 	{	
 		if(inventoryOpen){
-			inventoryButton.GetComponent<Transform>().Translate( new Vector3 (0f, -180.4f, 0f));
+			inventoryButton.GetComponent<Transform>().Translate( new Vector3 (0f, -174.4f, 0f));
 			inventoryBackground.SetActive (false);
 			inventoryOpen = false;
 
 		} else {
-			inventoryButton.GetComponent<Transform>().Translate(new Vector3 (0f, 180.4f, 0f));
+			inventoryButton.GetComponent<Transform>().Translate(new Vector3 (0f, 174.4f, 0f));
 
 			inventoryBackground.SetActive (true);
 			inventoryOpen = true;
