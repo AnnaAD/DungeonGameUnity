644dbc7e1da1727227acff02efe4b13360d90173
Ironbizmark <anna.arpacidusseau@gmail.com>
2018-09-03 07:18:07 -0500

    Fixed Pause Menu

---
+++ Dungeon^3/Assets/Scripts/WorldManager/ButtonClickController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickController : MonoBehaviour {
+
+    public void OnPauseClick() {
+        GameObject.Find("WorldManager").GetComponent<UImanager>().TogglePause();
+    }
+}
